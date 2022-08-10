4e2243f436c2f4e966c920c4ca800e6ab9c2f497
Rob <robstah72@gmail.com>
2022-08-10 18:09:34 +0100

    renamed state_update to update

--- basegamestate.py
+++ basegamestate.py
@@ -5,7 +5,7 @@ class BaseGameState:
     def state_init(self):
         pass
 
-    def state_update(self, dt_sec : float) -> None:
+    def update(self, dt_sec : float) -> None:
         pass
 
     def draw(self, screen: pygame.Surface) -> None:
