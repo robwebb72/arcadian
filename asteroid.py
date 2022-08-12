c3fdc2c7f9997ad00ccdca184cef8e95be3fa3f9
Rob <robstah72@gmail.com>
2022-08-12 17:29:51 +0100

    Changed asteroids to use bitmap images

--- asteroid.py
+++ asteroid.py
@@ -1,30 +1,34 @@
 import random
 import typing
+
 import pygame
-import colours
 
 
 class Asteroid:
-    def __init__(self, screen_size: typing.Tuple[int, int]) -> None:
+    def __init__(self, screen_size: typing.Tuple[int, int], image : pygame.Surface) -> None:
         self._screen_width, self._screen_height = screen_size
         self._speed_factor = 1.0
+        self._rect : pygame.Rect = pygame.Rect(0,0, 60, 60)
+        self._image = image
         self._create_new()
-        self._location_y = random.randrange(-self._screen_height, -self._size)
+        self.is_alive : bool = True
+        self._location_y = random.randrange(-self._screen_height, -self._rect.height)
+
+    def set_image(self, surface : pygame.Surface) -> None:
+        self._image = surface
+        self._rect = surface.get_rect()
 
     def update(self, dt_sec: float):
+        if self.is_alive == False:
+            return
         self._location_y += self._speed * dt_sec * self._speed_factor
-        if self._location_y > self._screen_height + self._size:
-            self._create_new()
-        self._position = (self._location_x, self._location_y)
+        if self._location_y > self._screen_height + self._rect.height:
+            self.is_alive = False
+        else:
+            self._position = (self._location_x, self._location_y)
 
     def draw(self, screen: pygame.Surface):
-        pygame.draw.circle(screen, colours.BLACK, self._position, self._size + 2)
-        pygame.draw.circle(screen, colours.GRAY, self._position, self._size)
-        offset_x = (int)(self._position[0] - self._size * 0.15)
-        offset_y = (int)(self._position[1] - self._size * 0.15)
-        pygame.draw.circle(
-            screen, colours.WHITE, (offset_x, offset_y), self._size * 0.6
-        )
+        screen.blit(self._image, self._position)
 
     def update_speed_factor(self, factor: float):
         self._speed_factor = factor
@@ -34,23 +38,37 @@ class Asteroid:
         self._size = size
 
     def _create_new(self):
-        self._size = random.randint(10, 70)
         self._speed = random.randrange(100, 200)
-        self._location_x = random.randint(self._size, self._screen_width - self._size)
-        self._location_y = -self._size
+        self._location_x = random.randint(self._rect.width, self._screen_width - self._rect.width)
+        self._location_y = -self._rect.height
         self._position = (self._location_x, self._location_y)
 
+    def create_new(self, surface : pygame.Surface) -> None:
+        self.set_image(surface)
+        self._create_new()
+        self.is_alive = True
 
 class AsteroidField:
     def __init__(self, nasteroids: int, screen_size: typing.Tuple) -> None:
+        self._images :typing.List[pygame.Surface]= []
+
+        for i in range(6):
+            image: pygame.Surface = pygame.image.load(f"images/asteroid{i}.png")
+            self._images.append(image)
+
         self._screen_width, self._screen_height = screen_size
         self._speed_factor = 1.0
         self._asteroids = []
         for i in range(nasteroids):
-            self._asteroids.append(Asteroid(screen_size))
+            self._asteroids.append(Asteroid(screen_size,random.choice(self._images)))
+
 
     def update(self, dt_sec: float) -> None:
-        [asteroid.update(dt_sec) for asteroid in self._asteroids]
+        for asteroid in self._asteroids:
+            if asteroid.is_alive:
+                asteroid.update(dt_sec)
+            else:
+                asteroid.create_new(random.choice(self._images))
 
     def draw(self, screen: pygame.Surface) -> None:
         [asteroid.draw(screen) for asteroid in self._asteroids]
