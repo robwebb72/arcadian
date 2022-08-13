88d21a4a38cfc244df7941d2ec305b940ec970d0
Rob <robstah72@gmail.com>
2022-08-13 13:01:45 +0100

    Added masked surface to asteroids

--- asteroid.py
+++ asteroid.py
@@ -2,77 +2,75 @@ import random
 import typing
 
 import pygame
-
+from maskedsurface import MaskedSurface
 
 class Asteroid:
     def __init__(
-        self, screen_size: typing.Tuple[int, int], image: pygame.Surface
+        self, screen_size: typing.Tuple[int, int], maskedSurface: MaskedSurface
     ) -> None:
-        self._screen_width, self._screen_height = screen_size
-        self._speed_factor = 1.0
-        self._rect: pygame.Rect = pygame.Rect(0, 0, 60, 60)
-        self._image = image
+        self._screen_width:int = screen_size[0]
+        self._screen_height:int = screen_size[1]
+
+        self._speed_factor: float = 1.0
+        self._maskedSurface: MaskedSurface= maskedSurface
         self._create_new()
         self.is_alive: bool = True
-        self._location_y = random.randrange(-self._screen_height, -self._rect.height)
+        self._location_y = random.randrange(-self._screen_height, -self._maskedSurface.rect.height)
 
-    def set_image(self, surface: pygame.Surface) -> None:
-        self._image = surface
-        self._rect = surface.get_rect()
+    def set_image(self, maskedSurface: MaskedSurface) -> None:
+        self._maskedSurface = maskedSurface
 
     def update(self, dt_sec: float):
         if self.is_alive == False:
             return
         self._location_y += self._speed * dt_sec * self._speed_factor
-        if self._location_y > self._screen_height + self._rect.height:
+        if self._location_y > self._screen_height + self._maskedSurface.rect.height:
             self.is_alive = False
         else:
             self._position = (self._location_x, self._location_y)
 
     def draw(self, screen: pygame.Surface):
-        screen.blit(self._image, self._position)
+        screen.blit(self._maskedSurface.surface, self._position)
 
     def update_speed_factor(self, factor: float):
         self._speed_factor = factor
 
-    def temp(self, position, size):
-        self._position = position
-        self._size = size
-
     def _create_new(self):
         self._speed = random.randrange(100, 200)
+        width = self._maskedSurface.rect.width
         self._location_x = random.randint(
-            self._rect.width, self._screen_width - self._rect.width
+            int(width/2), self._screen_width - int(width/2)
         )
-        self._location_y = -self._rect.height
+        self._location_y = -self._maskedSurface.rect.height
         self._position = (self._location_x, self._location_y)
 
-    def create_new(self, surface: pygame.Surface) -> None:
-        self.set_image(surface)
+    def create_new(self, maskedSurface: MaskedSurface) -> None:
+        self._maskedSurface = maskedSurface
         self._create_new()
         self.is_alive = True
 
 
 class AsteroidField:
     def __init__(self, nasteroids: int, screen_size: typing.Tuple) -> None:
-        self._images: typing.List[pygame.Surface] = []
+        self._surfaces: typing.List[MaskedSurface] = []
 
         for i in range(6):
-            image: pygame.Surface = pygame.image.load(f"images/asteroid{i}.png")
-            self._images.append(image)
+            surface: pygame.Surface = pygame.image.load(f"images/asteroid{i}.png");
+            maskedSurface: MaskedSurface = MaskedSurface(surface);
+            self._surfaces.append(maskedSurface)
 
         self._screen_width, self._screen_height = screen_size
         self._speed_factor = 1.0
         self._asteroids = []
         for i in range(nasteroids):
-            self._asteroids.append(Asteroid(screen_size, random.choice(self._images)))
+            self._asteroids.append(Asteroid(screen_size, random.choice(self._surfaces)))
 
     def update(self, dt_sec: float) -> None:
         for asteroid in self._asteroids:
             if asteroid.is_alive:
                 asteroid.update(dt_sec)
             else:
-                asteroid.create_new(random.choice(self._images))
+                asteroid.create_new(random.choice(self._surfaces))
 
     def draw(self, screen: pygame.Surface) -> None:
         [asteroid.draw(screen) for asteroid in self._asteroids]
--- maskedsurface.py
+++ maskedsurface.py
@@ -2,23 +2,7 @@ import pygame
 
 
 class MaskedSurface:
-    def __init__(self) -> None:
-        self.surface: pygame.Surface = None
-        self.mask: pygame.Mask = None
-        self.rect: pygame.Rect = None
-
     def __init__(self, surface: pygame.Surface) -> None:
         self.surface: pygame.Surface = surface
-        self.initialise()
-
-    def load(self, filename: str) -> None:
-        self.surface.pygame.image.load(filename)
-        self._initialise()
-
-    def from_surface(self, surface: pygame.Surface) -> None:
-        self.surface = surface
-        self._initialise()
-
-    def initialise(self) -> None:
-        self.mask = pygame.mask.from_surface(self.surface)
-        self.rect = self.surface.get_rect()
+        self.mask: pygame.mask.Mask = pygame.mask.from_surface(self.surface)
+        self.rect: pygame.Rect = self.surface.get_rect()
